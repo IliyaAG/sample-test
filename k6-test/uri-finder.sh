97c64e9efa2251b6e5b908d15d63129082b9966c
IliyaAG <iliyaagh76@gmail.com>
2025-09-05 15:12:27 +0330

    Add uri-finder.sh

---
+++ k6-test/uri-finder.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+DOMAIN=$1
+if [ -z "$1" ]; then
+    read -p "$(echo -e "${YELLOW}Enter your domain: ${RESET}")" DOMAIN
+else
+    DOMAIN=$1
+fi
+URIS=$(
+    curl -sL https://$DOMAIN | \
+    grep https://$DOMAIN | \
+    grep -oP '(?<=href=")[^"]+' | \
+    sed "s|https://$DOMAIN||" | \
+    sort -u
+)
+
+JS_URIS=$(echo "$URIS" |awk '{print "  \"" $0 "\","}')
+
+cat > variables.js.test <<EOL
+export const BASE_URL = 'https://$DOMAIN';
+
+export const paths = [
+$JS_URIS
+];
+EOL
