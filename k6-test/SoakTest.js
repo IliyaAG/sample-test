c016b49ee7b87349785096880b89c4cde300bc39
IliyaAG <iliyaagh76@gmail.com>
2025-09-08 16:53:26 +0330

    Add SoakTest

---
+++ k6-test/SoakTest.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+import { BASE_URL, urls } from './variables.js';
+
+let responseTime = new Trend('response_time');
+
+export let options = {
+  stages: [
+    { duration: '10m', target: 100 },
+    { duration: '4h', target: 100 },
+    { duration: '10m', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<1000'],
+    http_req_failed: ['rate<0.01'],
+  },
+};
+
+export default function () {
+  let endpoint = urls[Math.floor(Math.random() * urls.length)];
+  let res = http.get(`${BASE_URL}${endpoint}`, { tags: { endpoint } });
+
+  responseTime.add(res.timings.duration);
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'body is not empty': (r) => r.body && r.body.length > 0,
+  });
+
+  sleep(Math.random() * 3 + 1);
+}
